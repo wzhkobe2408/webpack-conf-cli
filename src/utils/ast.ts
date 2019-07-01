68d448d4e66d975bc8feb974d90e7602aa324464
王众欢 <wangzhonghuan@bytedance.com>
2019-07-01 20:35:23 +0800

    fix: add utility

---
+++ src/utils/ast.ts
@@ -0,0 +1,77 @@
+import * as esprima from 'esprima';
+import * as estraverse from 'estraverse';
+import * as escodegen from "escodegen";
+import * as fs from 'fs-extra';
+import * as path from 'path';
+
+const webpackConfigSourceCode = `
+"use strict";
+const path = require('path');
+module.exports = {
+    entry: {
+        index: [
+            './src/index.ts',
+        ]
+    },
+    output: {
+        filename: '[name].[hash:8].js',
+        path: path.join(__dirname, 'dist')
+    },
+    mode: 'development',
+    devtool: 'none',
+    devServer: {},
+    module: {
+        rules: []
+    },
+    resolve: {},
+    optimization: {},
+    plugins: []
+};
+`
+
+class WebpackConfigModifier {
+    modifyMode(node: any, parentNode: any, val: string) {
+        if (node.name === 'mode') {
+            parentNode.value.value = val;
+        }
+    }
+
+    modifyDevTool(node: any, parentNode: any, val: string) {
+        if (node.name === 'devtool') {
+            parentNode.value.value = val;
+        }
+    }
+
+    modifyModuleRules() {}
+}
+
+const webpackConfigModifier = new WebpackConfigModifier();
+
+/**
+ * STEP1: 解析成ast tree
+ */
+let ast = esprima.parseModule(webpackConfigSourceCode);
+
+/**
+ * STEP2: 遍历ast tree
+ * 访问者模式
+ */
+estraverse.traverse(ast, {
+    enter(node, parentNode) {
+        webpackConfigModifier.modifyMode(node, parentNode, 'production');
+        webpackConfigModifier.modifyDevTool(node, parentNode, 'source-map');
+    },
+    // leave(node) {}
+});
+
+/**
+ * STEP3: 生成新的code
+ */
+const generatedCode = escodegen.generate(ast);
+
+console.log('generatedCode', generatedCode);
+
+
+fs.writeFileSync(path.join(__dirname, '../webpack.config.js'), generatedCode, {
+    encoding: 'utf8',
+});
